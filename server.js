7eaeaeb269c789f8f00f9068f422e1a6a95492fc
Abilas Sathiyanesan <abilas96@gmail.com>
2021-12-30 00:04:28 -0500

    initial homepage

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const http = require('http');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  res.setHeader('Content-Type', 'text/html');
+
+  fs.readFile('./index.html', null, function(error, data) {
+    if (error) {
+      res.writeHead(404);
+      res.end('File not found')
+    }
+    else {
+      res.end(data);
+    }
+  })
 });
 
 server.listen(port, hostname, () => {
